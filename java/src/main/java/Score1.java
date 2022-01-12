4e252843701ebf1364b063da7fb4a4a57755c937
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-12 18:01:13 +0000

    Rename variables and remove braces from if statement

--- java/src/main/java/Score1.java
+++ java/src/main/java/Score1.java
@@ -1,56 +1,50 @@
 public class Score1 {
-    private int score1;
-    private int score2;
+    private int points1;
+    private int points2;
 
     public Score1(int score1, int score2) {
-        this.score1 = score1;
-        this.score2 = score2;
+        this.points1 = score1;
+        this.points2 = score2;
     }
 
     public String toString() {
-        if (tie()) {
-            return reportScoreForTie();
-        }
-        if (advantage()) {
-            return reportAdvantage();
-        }
-        if (win()) {
-            return reportWin();
-        }
+        if (tie()) return reportScoreForTie();
+        if (advantage()) return reportAdvantage();
+        if (win()) return reportWin();
         return reportScoreWithoutSpecialName();
     }
 
     public boolean tie() {
-        return score1 == score2;
+        return points1 == points2;
     }
 
     public boolean advantage() {
-        return (score1 > 3 || score2 > 3) && Math.abs(score1 - score2) == 1;
+        return (points1 > 3 || points2 > 3) && Math.abs(points1 - points2) == 1;
     }
 
     public boolean win() {
-        return (score1 > 3 || score2 > 3) && Math.abs(score1 - score2) >= 2;
+        return (points1 > 3 || points2 > 3) && Math.abs(points1 - points2) >= 2;
     }
 
     public String reportAdvantage() {
-        if (score1 - score2 > 0) return "Advantage player1";
+        if (points1 - points2 > 0) return "Advantage player1";
         return "Advantage player2";
     }
 
     public String reportWin() {
-        if (score1 - score2 > 0) return "Win for player1";
+        if (points1 - points2 > 0) return "Win for player1";
         return "Win for player2";
     }
 
     public String reportScoreForTie() {
-        if (score1 < 3) {
-            return toString(score1) + "-All";
+        if (points1 < 3) {
+            return toString(points1) + "-All";
         }
         return "Deuce";
     }
 
     public String reportScoreWithoutSpecialName() {
-        return toString(score1) + "-" + toString(score2);
+        return toString(points1) + "-" + toString(points2);
     }
 
     private static String toString(int score) {
